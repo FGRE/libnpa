4b37bc35bafc9e46d850c2433e42a5d42b5b98b7
Mislav Blažević <krofnica996@gmail.com>
2013-10-12 20:03:36 +0200

    Initial .nsb reading

--- include/nsbfile.hpp
+++ include/nsbfile.hpp
@@ -3,16 +3,45 @@
 
 #include <string>
 
+#define SWAP_UINT32(x) (((x) >> 24) | (((x) & 0x00FF0000) >> 8) | (((x) & 0x0000FF00) << 8) | ((x) << 24))
+
 enum OpenMode
 {
     NSB_COMPILED    = 1,
     NSB_PARSED      = 2
 };
 
+enum /* Magic */
+{
+    MAGIC_TEXT  = SWAP_UINT32(0xD8000300), // XML formatted text
+    MAGIC_DATA  = SWAP_UINT32(0xD0000200), // STRING/INT
+    MAGIC_BEGIN = SWAP_UINT32(0x94000100), // Begining of .nsb
+    MAGIC_IF    = SWAP_UINT32(0x98000100),
+    MAGIC_ENDIF = SWAP_UINT32(0xCF000100),
+
+    MAGIC_UNK1  = SWAP_UINT32(0xCE000100), // SkipOut/GameMainSet/SystemInit/InitSG/SetText/TypeBegin/TextBoxDelete
+    MAGIC_UNK2  = SWAP_UINT32(0xCE000200), // SetCharaColor/PhoneAutoOperation/PhoneOperation/PrintBG2
+    MAGIC_UNK3  = SWAP_UINT32(0xCE000300), // DeleteAllSt/CreatePlainSP/CreateProcessSG
+    MAGIC_UNK4  = SWAP_UINT32(0xCE000400), // FadeSt/DeleteSt/PosSt/BgCopy/BgLoad
+    MAGIC_UNK5  = SWAP_UINT32(0xCE000500), // FadeDelete/SoundPlay
+    MAGIC_UNK6  = SWAP_UINT32(0xCE000600), // St/ColorOut/SePlay
+
+    MAGIC_UNK7  = SWAP_UINT32(0xB0000100),
+    MAGIC_UNK8  = SWAP_UINT32(0xD1000100),
+
+    // Magic-only
+    MAGIC_UNK9  = SWAP_UINT32(0xC8000000),
+
+};
+
 class NsbFile
 {
 public:
 	NsbFile(std::string Name, OpenMode Mode);
+
+private:
+    void ReadFromSource(std::string& Name);
+    void ReadFromBinary(std::string& Name);
 };
 
 #endif
--- src/nsbfile.cpp
+++ src/nsbfile.cpp
@@ -1,6 +1,8 @@
 #include "nsbfile.hpp"
 
 #include <cassert>
+#include <fstream>
+#include <iostream>
 
 NsbFile::NsbFile(std::string Name, OpenMode Mode)
 {
@@ -12,4 +14,47 @@ NsbFile::NsbFile(std::string Name, OpenMode Mode)
         default:
             assert(false);
     }
+}
+
+void ReadFromSource(std::string& Name)
+{
+}
+
+void ReadFromBinary(std::string& Name)
+{
+    std::ifstream File(Name, std::ios::in | std::ios::binary);
+    uint32_t Entry, Magic, Length;
+    char* String = nullptr;
+
+    while (File.read((char*)&Entry, sizeof(uint32_t)))
+    {
+        File.read((char*)&Magic, sizeof(uint32_t));
+        switch (Magic)
+        {
+            case MAGIC_UNK1:
+            case MAGIC_UNK2:
+            case MAGIC_UNK3:
+            case MAGIC_UNK4:
+            case MAGIC_UNK5:
+            case MAGIC_UNK6:
+            case MAGIC_UNK7:
+            case MAGIC_UNK8:
+
+            case MAGIC_IF:
+            case MAGIC_ENDIF:
+            case MAGIC_BEGIN:
+            case MAGIC_TEXT:
+            case MAGIC_DATA:
+                File.read((char*)&Length, sizeof(uint32_t));
+                delete[] String;
+                String = new char[Length];
+                File.read(String, Length);
+                break;
+            default:
+                std::cout << "Unknown magic: " << std::hex << Magic << std::endl;
+                assert(false);
+        }
+    }
+
+    delete[] String;
 }
