254bb0fe45b8dc53053793a0bb8f7b7e0c6eb3ed
Mislav Blažević <krofnica996@gmail.com>
2013-10-13 17:10:27 +0200

    .nsb script parser is now functional (but incomplete)

--- include/nsbfile.hpp
+++ include/nsbfile.hpp
@@ -2,8 +2,9 @@
 #define NSB_FILE_HPP
 
 #include <string>
+#include <vector>
 
-#define SWAP_UINT32(x) (((x) >> 24) | (((x) & 0x00FF0000) >> 8) | (((x) & 0x0000FF00) << 8) | ((x) << 24))
+#define SWAP_UINT16(x) (((x) >> 8) | ((x) << 8))
 
 enum OpenMode
 {
@@ -13,35 +14,76 @@ enum OpenMode
 
 enum /* Magic */
 {
-    MAGIC_TEXT  = SWAP_UINT32(0xD8000300), // XML formatted text
-    MAGIC_DATA  = SWAP_UINT32(0xD0000200), // STRING/INT
-    MAGIC_BEGIN = SWAP_UINT32(0x94000100), // Begining of .nsb
-    MAGIC_IF    = SWAP_UINT32(0x98000100),
-    MAGIC_ENDIF = SWAP_UINT32(0xCF000100),
-
-    MAGIC_UNK1  = SWAP_UINT32(0xCE000100), // SkipOut/GameMainSet/SystemInit/InitSG/SetText/TypeBegin/TextBoxDelete
-    MAGIC_UNK2  = SWAP_UINT32(0xCE000200), // SetCharaColor/PhoneAutoOperation/PhoneOperation/PrintBG2
-    MAGIC_UNK3  = SWAP_UINT32(0xCE000300), // DeleteAllSt/CreatePlainSP/CreateProcessSG
-    MAGIC_UNK4  = SWAP_UINT32(0xCE000400), // FadeSt/DeleteSt/PosSt/BgCopy/BgLoad
-    MAGIC_UNK5  = SWAP_UINT32(0xCE000500), // FadeDelete/SoundPlay
-    MAGIC_UNK6  = SWAP_UINT32(0xCE000600), // St/ColorOut/SePlay
-
-    MAGIC_UNK7  = SWAP_UINT32(0xB0000100),
-    MAGIC_UNK8  = SWAP_UINT32(0xD1000100),
+    MAGIC_TEXT  = SWAP_UINT16(0xD800), // XML formatted text/voice
+    MAGIC_PARAM = SWAP_UINT16(0xD000), // STRING/INT
+    MAGIC_BEGIN = SWAP_UINT16(0x9400), // Begining of .nsb
+    MAGIC_IF    = SWAP_UINT16(0x9800), // label.if
+    MAGIC_ENDIF = SWAP_UINT16(0xCF00), // label.if.end
+    MAGIC_CALL  = SWAP_UINT16(0xCE00), // Function call
 
     // Magic-only
-    MAGIC_UNK9  = SWAP_UINT32(0xC8000000),
+    MAGIC_UNK1  = SWAP_UINT16(0xC600),
+    MAGIC_UNK2  = SWAP_UINT16(0xC700),
+    MAGIC_UNK3  = SWAP_UINT16(0xC800),
+    MAGIC_UNK4  = SWAP_UINT16(0xC900),
+    MAGIC_UNK5  = SWAP_UINT16(0x6800),
+    MAGIC_UNK6  = SWAP_UINT16(0x8E00),
+    MAGIC_UNK7  = SWAP_UINT16(0x5B00),
+    MAGIC_UNK8  = SWAP_UINT16(0xC100),
+    MAGIC_UNK9  = SWAP_UINT16(0xA500),
 
+    MAGIC_UNK10 = SWAP_UINT16(0xB000), // $GameName/$GameContinue
+    MAGIC_UNK11 = SWAP_UINT16(0xD100), // $GameStart/#SYSTEM
+    MAGIC_UNK12 = SWAP_UINT16(0xD400),
+    MAGIC_UNK13 = SWAP_UINT16(0x9500), // scene.sg00_01.nss/scene.sg00_01.nss_MAIN
+    MAGIC_UNK14 = SWAP_UINT16(0xA300), // $MainGameName
+    MAGIC_UNK15 = SWAP_UINT16(0xD500), // scene.sg00_01.nss_MAIN
+    MAGIC_UNK16 = SWAP_UINT16(0x2000),
+    MAGIC_UNK17 = SWAP_UINT16(0x2300),
+    MAGIC_UNK18 = SWAP_UINT16(0x2400),
+    MAGIC_UNK19 = SWAP_UINT16(0x2500),
+    MAGIC_UNK20 = SWAP_UINT16(0x3900),
+    MAGIC_UNK21 = SWAP_UINT16(0x3A00),
+    MAGIC_UNK22 = SWAP_UINT16(0x9100),
+    MAGIC_UNK23 = SWAP_UINT16(0xD200),
+    MAGIC_UNK24 = SWAP_UINT16(0x4300),
+    MAGIC_UNK25 = SWAP_UINT16(0x2800),
+    MAGIC_UNK26 = SWAP_UINT16(0xDE00),
+    MAGIC_UNK27 = SWAP_UINT16(0xE400),
+    MAGIC_UNK28 = SWAP_UINT16(0x1400),
+    MAGIC_UNK29 = SWAP_UINT16(0x2B00),
+    MAGIC_UNK30 = SWAP_UINT16(0x1200),
+    MAGIC_UNK31 = SWAP_UINT16(0x2C00),
+    MAGIC_UNK32 = SWAP_UINT16(0x2D00),
+    MAGIC_UNK33 = SWAP_UINT16(0x1B00),
+    MAGIC_UNK34 = SWAP_UINT16(0x3000),
+    MAGIC_UNK35 = SWAP_UINT16(0xC000),
+    MAGIC_UNK36 = SWAP_UINT16(0x9700),
+    MAGIC_UNK37 = SWAP_UINT16(0xD700), // Ending of .nsb
+};
+
+struct Line
+{
+    uint16_t Magic;
+    std::vector<std::string> Params;
 };
 
 class NsbFile
 {
 public:
-	NsbFile(std::string Name, OpenMode Mode);
+	NsbFile(const std::string& Name, OpenMode Mode);
+
+    static const char* StringifyMagic(uint16_t Magic);
+    Line* GetNextLine();
+    std::string GetName();
 
 private:
-    void ReadFromSource(std::string& Name);
-    void ReadFromBinary(std::string& Name);
+    void ReadFromSource();
+    void ReadFromBinary();
+
+    uint32_t SourceIter;
+    std::vector<Line> Source;
+    std::string Name;
 };
 
 #endif
--- src/nsbfile.cpp
+++ src/nsbfile.cpp
@@ -2,59 +2,126 @@
 
 #include <cassert>
 #include <fstream>
-#include <iostream>
+#include <boost/assign/list_of.hpp>
+#include <boost/unordered_map.hpp>
 
-NsbFile::NsbFile(std::string Name, OpenMode Mode)
+static const boost::unordered_map<uint16_t, const char*> MagicStrings = boost::assign::map_list_of
+    (MAGIC_TEXT, "TEXT")
+    (MAGIC_PARAM, "PARAM")
+    (MAGIC_BEGIN, "BEGIN")
+    (MAGIC_IF, "IF")
+    (MAGIC_ENDIF, "ENDIF")
+    (MAGIC_CALL, "CALL")
+
+    (MAGIC_UNK1, "UNK1")
+    (MAGIC_UNK2, "UNK2")
+    (MAGIC_UNK3, "UNK3")
+    (MAGIC_UNK4, "UNK4")
+    (MAGIC_UNK5, "UNK5")
+    (MAGIC_UNK6, "UNK6")
+    (MAGIC_UNK7, "UNK7")
+    (MAGIC_UNK8, "UNK8")
+    (MAGIC_UNK9, "UNK9")
+
+    (MAGIC_UNK10, "UNK10")
+    (MAGIC_UNK11, "UNK11")
+    (MAGIC_UNK12, "UNK12")
+    (MAGIC_UNK13, "UNK13")
+    (MAGIC_UNK14, "UNK14")
+    (MAGIC_UNK15, "UNK15")
+    (MAGIC_UNK16, "UNK16")
+    (MAGIC_UNK17, "UNK17")
+    (MAGIC_UNK18, "UNK18")
+    (MAGIC_UNK19, "UNK19")
+    (MAGIC_UNK20, "UNK20")
+    (MAGIC_UNK21, "UNK21")
+    (MAGIC_UNK22, "UNK22")
+    (MAGIC_UNK23, "UNK23")
+    (MAGIC_UNK24, "UNK24")
+    (MAGIC_UNK25, "UNK25")
+    (MAGIC_UNK26, "UNK26")
+    (MAGIC_UNK27, "UNK27")
+    (MAGIC_UNK28, "UNK28")
+    (MAGIC_UNK29, "UNK29")
+    (MAGIC_UNK30, "UNK30")
+    (MAGIC_UNK31, "UNK31")
+    (MAGIC_UNK32, "UNK32")
+    (MAGIC_UNK33, "UNK33")
+    (MAGIC_UNK34, "UNK34")
+    (MAGIC_UNK35, "UNK35")
+    (MAGIC_UNK36, "UNK36")
+    (MAGIC_UNK37, "UNK37");
+
+/* PUBLIC */
+
+NsbFile::NsbFile(const std::string& Name, OpenMode Mode) :
+SourceIter(0),
+Name(Name)
 {
     switch (Mode)
     {
         case NSB_COMPILED:
+            ReadFromBinary();
+            break;
         case NSB_PARSED:
+            ReadFromSource();
             break;
         default:
             assert(false);
     }
 }
 
-void ReadFromSource(std::string& Name)
+const char* NsbFile::StringifyMagic(uint16_t Magic)
+{
+    return MagicStrings.at(Magic);
+}
+
+Line* NsbFile::GetNextLine()
+{
+    return SourceIter < Source.size() ? &Source[SourceIter++] : nullptr;
+}
+
+std::string NsbFile::GetName()
+{
+    return Name;
+}
+
+/* PRIVATE */
+
+void NsbFile::ReadFromSource()
 {
 }
 
-void ReadFromBinary(std::string& Name)
+void NsbFile::ReadFromBinary()
 {
     std::ifstream File(Name, std::ios::in | std::ios::binary);
-    uint32_t Entry, Magic, Length;
-    char* String = nullptr;
+    uint32_t Entry, Length;
+    uint16_t NumParams;
+    Line* CurrLine;
 
+    // Find # of lines
+    File.seekg(-2 * sizeof(uint32_t), File.end);
+    File.read((char*)&Entry, sizeof(uint32_t));
+    Source.resize(Entry);
+    File.seekg(0, File.beg);
+
+    // Read source code lines
     while (File.read((char*)&Entry, sizeof(uint32_t)))
     {
-        File.read((char*)&Magic, sizeof(uint32_t));
-        switch (Magic)
-        {
-            case MAGIC_UNK1:
-            case MAGIC_UNK2:
-            case MAGIC_UNK3:
-            case MAGIC_UNK4:
-            case MAGIC_UNK5:
-            case MAGIC_UNK6:
-            case MAGIC_UNK7:
-            case MAGIC_UNK8:
+        CurrLine = &Source[Entry - 1];
+        File.read((char*)&CurrLine->Magic, sizeof(uint16_t));
+        File.read((char*)&NumParams, sizeof(uint16_t));
+        CurrLine->Params.reserve(NumParams);
 
-            case MAGIC_IF:
-            case MAGIC_ENDIF:
-            case MAGIC_BEGIN:
-            case MAGIC_TEXT:
-            case MAGIC_DATA:
-                File.read((char*)&Length, sizeof(uint32_t));
-                delete[] String;
-                String = new char[Length];
-                File.read(String, Length);
-                break;
-            default:
-                std::cout << "Unknown magic: " << std::hex << Magic << std::endl;
-                assert(false);
+        // Read parameters
+        for (uint16_t i = 0; i < NumParams; ++i)
+        {
+            File.read((char*)&Length, sizeof(uint32_t));
+            char* String = new char[Length + 1];
+            File.read(String, Length);
+            String[Length] = 0;
+            CurrLine->Params.push_back(String);
+            delete[] String;
         }
     }
-
-    delete[] String;
 }
