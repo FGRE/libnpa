7dd566924689b451a93d80a54445346ca5498552
Mislav Blažević <krofnica996@gmail.com>
2014-07-19 11:03:14 +0200

    Fix compatibility with flex 2.5.35 or older

--- src/nsscompiler.cpp
+++ src/nsscompiler.cpp
@@ -10,7 +10,13 @@ extern Block* pRoot;
 extern int yyparse();
 typedef struct yy_buffer_state* YY_BUFFER_STATE;
 extern int yyparse();
+#if ((FLEX_VERSION_MAJOR) > 2 || \
+    ((FLEX_VERSION_MAJOR == 2 && FLEX_VERSION_MINOR > 5) || \
+    ((FLEX_VERSION_MAJOR == 2 && FLEX_VERSION_MINOR == 5 && FLEX_VERSION_PATCH > 35))))
 extern YY_BUFFER_STATE yy_scan_bytes(const char* bytes, size_t len);
+#else
+extern YY_BUFFER_STATE yy_scan_bytes(const char* bytes, int len);
+#endif
 extern void yy_delete_buffer(YY_BUFFER_STATE buffer);
 
 static uint32_t Counter = 1;
