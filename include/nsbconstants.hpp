31ff5e4c90755fbc7804d03d61712362f1bf24a0
Mislav Blažević <krofnica996@gmail.com>
2014-12-03 21:16:29 +0100

    Add ~700 script constants for use in nptools and libnpengine

---
+++ include/nsbconstants.hpp
@@ -0,0 +1,42 @@
+namespace Nsb
+{
+    enum Request
+    {
+        STOP,
+        PLAY,
+        DISUSED,
+        ADD_RENDER,
+        LOCK,
+        ERASE,
+        UN_CLICKABLE,
+        UN_LOCK,
+        START,
+        ENTER,
+        PASSIVE,
+        SMOOTHING,
+        NO_LOG,
+        PUSH_TEXT,
+        NO_ICON,
+        HIDEABLE,
+        PAUSE,
+        RESUME,
+        ENTRUST_SUSPEND,
+        COMPULSORY_SUSPEND,
+        SUB_RENDER
+    };
+
+    enum Tempo
+    {
+        AXL_1,
+        AXL_2,
+        AXL_3,
+        DXL_1,
+        DXL_2,
+        DXL_3,
+        AXL_DXL,
+        NSB_NULL = -1
+    };
+
+    bool IsValidConstant(const std::string& String);
+    int32_t ConstantToValue(const std::string& String);
+}
